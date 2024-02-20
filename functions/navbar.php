68d44a8ebe1f90f1c5a67d5a173ac66a159ced97
Jose Santos <143058089+josesnts@users.noreply.github.com>
2024-02-20 11:43:04 +0800

    Delete functions directory

--- functions/navbar.php
+++
@@ -1,8 +0,0 @@
-<!-- NavBar Brand -->
-<a class="navbar-brand" href="index.php">
-    <img src="images/Jose-Logo-light.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-center animate__animated animate__flip">
-</a>
-<!-- Toggler Button -->
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-</button>
